5eb58ea18aaee1459682a202444e7819fd2a408c
Bhargav Joshi <82135781+bhargav-18@users.noreply.github.com>
2021-08-03 14:25:18 +0530

    Gradle upgrade

---
+++ app/src/main/java/com/project/findme/SplashScreenFragment.kt
@@ -0,0 +1,13 @@
+package com.project.findme
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.ryan.findme.R
+
+class SplashScreenFragment : Fragment(R.layout.fragment_splash_screen) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/project/findme/auth/ui/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.project.findme.auth.ui
+
+import androidx.fragment.app.Fragment
+import com.ryan.findme.R
+
+class LoginFragment : Fragment(R.layout.fragment_login_user) {
+}
