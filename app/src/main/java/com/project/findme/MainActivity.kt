f72765d6c3b38bcb4219107f4bf4d64c6caa6e30
Bhargav Joshi <82135781+bhargav-18@users.noreply.github.com>
2021-08-03 20:43:56 +0530

    nav

--- app/src/main/java/com/project/findme/MainActivity.kt
+++ app/src/main/java/com/project/findme/MainActivity.kt
@@ -1,12 +1,60 @@
 package com.project.findme
 
+import android.app.NotificationManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.ryan.findme.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var bottomNavBar: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavBar = findViewById(R.id.bottom_nav_view)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancelAll()
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        val navView = findViewById<NavigationView>(R.id.side_nav_view)
+
+        appBarConfiguration = AppBarConfiguration.Builder(R.id.splashScreenFragment).setOpenableLayout(drawerLayout)
+            .build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavBar.setupWithNavController(navController)
+        navView.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            navController,
+            appBarConfiguration
+        ) || super.onSupportNavigateUp()
     }
 }
