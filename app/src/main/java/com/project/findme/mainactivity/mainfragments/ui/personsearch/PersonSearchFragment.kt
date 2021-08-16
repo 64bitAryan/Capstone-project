3c5a9e9ecabef4f917d31e922527959d7ffbffe9
Bhargav Joshi <82135781+bhargav-18@users.noreply.github.com>
2021-08-16 14:28:52 +0530

    search final commit

--- app/src/main/java/com/project/findme/mainactivity/mainfragments/ui/personsearch/PersonSearchFragment.kt
+++ app/src/main/java/com/project/findme/mainactivity/mainfragments/ui/personsearch/PersonSearchFragment.kt
@@ -8,24 +8,18 @@ import androidx.core.widget.addTextChangedListener
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
 import com.project.findme.adapter.UserAdapter
-import com.project.findme.data.entity.User
 import com.project.findme.utils.Constants.SEARCH_TIME_DELAY
 import com.project.findme.utils.EventObserver
 import com.project.findme.utils.snackbar
 import com.ryan.findme.R
 import com.ryan.findme.databinding.FragmentSearchPersonBinding
-import com.squareup.okhttp.Dispatcher
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -46,7 +40,7 @@ class PersonSearchFragment : Fragment(R.layout.fragment_search_person) {
         binding.etSearch.setText(viewModel.searchQuery)
 
         var job: Job? = null
-        binding.etSearch.doOnTextChanged { text, _, _, _ ->
+        binding.etSearch.addTextChangedListener { text ->
             viewModel.searchQuery = text.toString()
             job?.cancel()
             job = lifecycleScope.launch {
@@ -74,7 +68,12 @@ class PersonSearchFragment : Fragment(R.layout.fragment_search_person) {
             }
         ) { userList ->
             binding.searchProgressbar.isVisible = false
-            userAdapter.users = userList
+            if(userList.isEmpty()){
+                snackbar("No such User found")
+            }
+            else{
+                userAdapter.users = userList
+            }
         })
     }
 
