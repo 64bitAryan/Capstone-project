a21af5ecfbec3f5b86460b1bdd78ecbba32f3484
Aryan patel <patelaryan1726@gmail.com>
2021-08-16 15:04:55 +0530

    Side commit

--- app/src/main/java/com/project/findme/mainactivity/mainfragments/ui/settings/SettingsFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.project.findme.mainactivity.mainfragments.ui.settings
-
-import androidx.fragment.app.Fragment
-import com.ryan.findme.R
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class SettingsFragment : Fragment(R.layout.fragment_settings_screen){
-}
---
+++ app/src/main/java/com/project/findme/mainactivity/mainfragments/ui/userProfile/UserProfileFragment.kt
@@ -0,0 +1,8 @@
+package com.project.findme.mainactivity.mainfragments.ui.userProfile
+
+import androidx.fragment.app.Fragment
+import com.ryan.findme.R
+
+class UserProfileFragment: Fragment(R.layout.fragment_user_profile) {
+
+}
