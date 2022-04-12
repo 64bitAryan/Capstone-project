06853b92275674a803910cfe5e06382fe26bde53
Bhargav Joshi <82135781+bhargav-18@users.noreply.github.com>
2022-04-12 21:15:48 +0530

    camera feature done in create post

--- app/src/main/java/com/project/findme/mainactivity/mainfragments/ui/createPost/CreatePostFragment.kt
+++ app/src/main/java/com/project/findme/mainactivity/mainfragments/ui/createPost/CreatePostFragment.kt
@@ -1,30 +1,20 @@
 package com.project.findme.mainactivity.mainfragments.ui.createPost
 
-import android.content.Context
-import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.ImageDecoder
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
-import android.widget.ImageView
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContract
-import androidx.annotation.RequiresApi
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.RequestManager
+import com.canhub.cropper.*
 import com.project.findme.utils.EventObserver
 import com.project.findme.utils.snackbar
 import com.ryan.findme.R
 import com.ryan.findme.databinding.FragmentCreatepostScreenBinding
-import com.theartofdev.edmodo.cropper.CropImage
-import com.theartofdev.edmodo.cropper.CropImageView
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -34,41 +24,20 @@ class CreatePostFragment : Fragment(R.layout.fragment_createpost_screen) {
     @Inject
     lateinit var glide: RequestManager
     private val viewModel: CreatePostViewModel by viewModels()
-    private lateinit var cropContent: ActivityResultLauncher<Any?>
-    lateinit var curImageUri: Uri
+    private lateinit var curImageUri: Uri
     private lateinit var binding: FragmentCreatepostScreenBinding
-    private lateinit var mYourBitmap: Bitmap
 
-    private val cropActivityResultContract = object : ActivityResultContract<Any?, Uri?>() {
-        override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
-            return CropImage.getActivityResult(intent)?.uri
-        }
-
-        override fun createIntent(context: Context, input: Any?): Intent {
-            return CropImage.activity()
-                .setAspectRatio(1, 1)
-                .getIntent(context)
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.P)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentCreatepostScreenBinding.bind(view)
         subscribeToObserve()
+
         binding.apply {
-
-            cropContent = registerForActivityResult(cropActivityResultContract) {
-                it?.let { uri ->
-                    viewModel.setCurrentImageUri(uri)
-                }
-            }
-
             addImageBt.setOnClickListener {
-                cropContent.launch(null)
+                startCrop()
             }
             imageIv.setOnClickListener {
-                cropContent.launch(null)
+                startCrop()
             }
             createPostBt.setOnClickListener {
                 curImageUri.let { uri ->
@@ -81,6 +50,26 @@ class CreatePostFragment : Fragment(R.layout.fragment_createpost_screen) {
             }
         }
     }
+
+    private val cropImage = registerForActivityResult(CropImageContract()) { result ->
+        if (result.isSuccessful) {
+            curImageUri = result.uriContent!!
+            viewModel.setCurrentImageUri(curImageUri)
+        } else {
+            val exception = result.error
+            snackbar(exception.toString())
+        }
+    }
+
+    private fun startCrop() {
+        cropImage.launch(
+            options {
+                setGuidelines(CropImageView.Guidelines.ON)
+                setAspectRatio(1, 1)
+            }
+        )
+    }
+
 
     private fun subscribeToObserve() {
 
